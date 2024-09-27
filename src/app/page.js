c0f053cdd6e03ae16a774a88f0f7d4b1f104d622
qBlackbirdp <blackbird9711@gmail.com>
2024-09-27 16:42:34 +0900

    MUI 추가

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    Hello Wolrd!
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex"></div>
+        Hello Wolrd!
+      </main>
     </>
-      );
+  );
 }
