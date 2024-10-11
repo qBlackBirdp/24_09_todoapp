177e9f04cc7ac2ef40a50a9db75dfa4c566be964
qBlackbirdp <blackbird9711@gmail.com>
2024-10-11 14:34:01 +0900

    멀티라인 텍스트, Box

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import {
   createTheme,
   CssBaseline,
   Chip,
+  Box,
 } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { FaBars } from 'react-icons/fa';
@@ -186,6 +187,8 @@ const App = () => {
       <Toolbar />
       <form className="tw-flex tw-flex-col tw-p-4 tw-gap-2" onSubmit={onSubmit}>
         <TextField
+          multiline
+          maxRows={4}
           name="content"
           id="outlined-basic"
           label="할 일 입력"
@@ -202,9 +205,18 @@ const App = () => {
           {todosState.todos.map((todo) => (
             <li className="tw-mb-3" key={todo.id}>
               <div className="tw-flex tw-flex-col tw-gap-1 tw-mb-[30px]">
-                <Chip label={`번호 : ${todo.id}`} variant="outlined"></Chip>
-                <Chip label={`날짜 : ${todo.regDate}`} variant="outlined"></Chip>
-                <div className='tw-p-8 tw-rounded-[15px] tw=shadow tw-whitespace-pre-wrap'>할 일 : {todo.content}</div>
+                <Chip
+                  className="tw-pt-3"
+                  label={`번호 : ${todo.id}`}
+                  variant="outlined"
+                  color="secondary"></Chip>
+                <Chip
+                  className="tw-pt-3"
+                  label={`날짜 : ${todo.regDate}`}
+                  variant="outlined"></Chip>
+                <div className="tw-p-8 tw-rounded-[15px] tw-shadow tw-whitespace-pre-wrap tw-leading-relaxed tw-break-words">
+                  <Box sx={{ color: 'secondary.dark' }}>할 일 : {todo.content}</Box>
+                </div>
               </div>
             </li>
           ))}
--- src/app/theme.js
+++ src/app/theme.js
@@ -6,6 +6,7 @@ const theme = createTheme({
     type: 'light',
     primary: {
       main: '#f50057',
+      dark: '#afafaf',
     },
     secondary: {
       main: '#f50057',
